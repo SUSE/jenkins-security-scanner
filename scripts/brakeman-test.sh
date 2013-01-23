cef721661671de478a677a0614cb68838644d88b
Lukas Ocilka <lukas.ocilka@gmail.com>
2013-01-23 11:44:11 +0100

    Added support for more projects being checked on one Jenkins node

--- scripts/brakeman-test.sh
+++ scripts/brakeman-test.sh
@@ -1,23 +1,34 @@
 # The only parameter entered by user is a directory containing
-# the actual Rails application
+# the actual Rails application - probably doesn't work if
+# it contains whitespaces or other special characters
 TEST_DIR=$1
 
 if [ "$TEST_DIR" == "" ]; then
-  echo "Usage: $0 path-to-directory-with-the-rails-app"
+  echo "Usage: $0 path-to-directory-with-the-rails-app project-id-or-name"
   exit 1
 fi
 
+# Project ID or name - has to be unique within projects tested
+# on this node otherwise the HTML and JSON output would be mixed
+PROJECT_ID=$2
+
+if [ "$PROJECT_ID" == "" ]; then
+  echo "Usage: $0 path-to-directory-with-the-rails-app project-id-or-name"
+  exit 1
+fi
+
+
 # All file paths are relative to workspace (working directory)
 PERSISTENT_DIR="__JENKINS_TEST__"
-THIS_BUILD_DIR="${PERSISTENT_DIR}/builds/${BUILD_NUMBER}"
+THIS_BUILD_DIR="${PERSISTENT_DIR}/${PROJECT_ID}/builds/${BUILD_NUMBER}"
 
-LAST_REPORT_JSON="${PERSISTENT_DIR}/last-brakeman-report.json"
-LAST_REPORT_HTML="${PERSISTENT_DIR}/last-brakeman-report.html"
-LAST_CHANGES_JSON="${PERSISTENT_DIR}/last-changes.json"
+LAST_REPORT_JSON="${PERSISTENT_DIR}/${PROJECT_ID}/last-brakeman-report.json"
+LAST_REPORT_HTML="${PERSISTENT_DIR}/${PROJECT_ID}/last-brakeman-report.html"
+LAST_CHANGES_JSON="${PERSISTENT_DIR}/${PROJECT_ID}/last-changes.json"
 
-CURRENT_REPORT_HTML="${THIS_BUILD_DIR}/brakeman-report.html"
-CURRENT_REPORT_JSON="${THIS_BUILD_DIR}/brakeman-report.json"
-CURRENT_CHANGES_JSON="${THIS_BUILD_DIR}/changes.json"
+CURRENT_REPORT_HTML="${THIS_BUILD_DIR}/${PROJECT_ID}/brakeman-report.html"
+CURRENT_REPORT_JSON="${THIS_BUILD_DIR}/${PROJECT_ID}/brakeman-report.json"
+CURRENT_CHANGES_JSON="${THIS_BUILD_DIR}/${PROJECT_ID}/changes.json"
 
 # Creates brakeman summary visible in build log
 brakeman $TEST_DIR
